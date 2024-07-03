661b02c7fc6f157325d8e2854833c4f5a40ac33a
cabbaeg <suprisedcabbage@gmail.com>
2024-07-03 11:08:22 -0700

    minor edits, only accepts images now

--- Frontend/pages/Uploads.py
+++ Frontend/pages/Uploads.py
@@ -7,15 +7,15 @@ uploads_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'up
 # Ensure the 'uploads' directory exists
 if not os.path.exists(uploads_dir):
     os.makedirs(uploads_dir)
-    
+
 st.set_page_config(page_title="Uploads")
 # Ensure the 'uploads' directory exists
 # if not os.path.exists('uploads'):
 #     os.makedirs('uploads')
 
-st.title('File Upload Example')
+st.title('Image Upload')
 
-uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
+uploaded_files = st.file_uploader("Choose image files", type=["png", "jpg", "jpeg", "gif"], accept_multiple_files=True)
 
 if uploaded_files:
     for uploaded_file in uploaded_files:

24a03b63629e823f70aa5e2e8d85d3010d892ba0
cabbaeg <suprisedcabbage@gmail.com>
2024-07-03 10:45:46 -0700

    this is a cry for help

--- Frontend/pages/Uploads.py
+++ Frontend/pages/Uploads.py
@@ -3,8 +3,8 @@ import os
 
 st.set_page_config(page_title="Uploads")
 # Ensure the 'uploads' directory exists
-if not os.path.exists('Frontend/uploads'):
-    os.makedirs('Frontend/uploads')
+# if not os.path.exists('uploads'):
+#     os.makedirs('uploads')
 
 st.title('File Upload Example')
 
@@ -12,8 +12,8 @@ uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
 
 if uploaded_files:
     for uploaded_file in uploaded_files:
-        save_path = os.path.join('Frontend/uploads', uploaded_file.name)
+        save_path = os.path.join('uploads', uploaded_file.name)
         with open(save_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
         st.success(f"Saved file: {uploaded_file.name}")
-        st.image('Frontend/uploads/'+uploaded_file.name, caption='lorem ipsum')
+        st.image('uploads/'+uploaded_file.name, caption='lorem ipsum')

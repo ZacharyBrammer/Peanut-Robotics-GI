4e8638061b46f897e72f370e9f20ccbc7dc95bc9
co16661666 <62578553+co16661666@users.noreply.github.com>
2024-07-03 17:57:41 -0400

    Created schema, working on uploading to Lance
    Co-authored-by: Skyami404 <Skyami404@users.noreply.github.com>
    Co-authored-by: carly11110 <carly11110@users.noreply.github.com>
    Co-authored-by: Microwaved Potato <MicrowavedFood@users.noreply.github.com>
    Co-authored-by: nima-bhavansikar <nima-bhavansikar@users.noreply.github.com>
    Co-authored-by: alice439 <alice439@users.noreply.github.com>

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ class Animal(Enum):
     timber_wolf = 14
 
 
-print(testImagePath[0])
-print(Animal(testImagePath[0]["labels"]).name)
+# print(testImagePath[0])
+# print(Animal(testImagePath[0]["labels"]).name)
 
 # load the pretrained model
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -53,118 +53,159 @@ response = requests.get("https://www.thespruce.com/thmb/iMt63n8NGCojUETr6-T8oj-5
 img = Image.open(BytesIO(response.content))
 
 emb_img = gen_embeddings(img)
-print("emebedded test image: ")
-print(emb_img)
+# print("emebedded test image: ")
+# print(emb_img)
 
-# set up lancedb table
+# reads trajectory data file
+def read_trajectory(path):
+    # trajectory_data = pandas.read_csv(path)
+    trajectory_data = "placeholder"
+    return trajectory_data
 
+# def store_data(db, image_path, embeddings, trajectory_data):
+#     db.insert('image_embeddings', {
+#         'image_path': image_path,
+#         'embedding': embeddings.tolist(),
+#         'trajectory_data': trajectory_data
+#     })
 
-db = lancedb.connect("./data/tables")
-schema = pa.schema(
-    [
-        pa.field("vector", pa.list_(pa.float32(), 512)),
-        pa.field("id", pa.int32()),
-        pa.field("label", pa.int32()),
-    ]
-)
-tbl = db.create_table("images", schema=schema, mode="overwrite")
+def process_images(dir_path, trajectory_path):
+    # db = lancedb.LanceDb('embeddings.db')
+    db = lancedb.connect('embeddings.db')
+    # db.create_table('image_embeddings', ['image_path', 'embedding', 'trajectory_data'])
 
-data = []
-for i in tqdm(range(1, len(testImagePath))):
-    data.append(
-        {"vector": gen_embeddings(testImagePath[i]["img"]), "id": i, "label": testImagePath[i]["labels"]}
+    # tbl = db.create_table('image_embeddings', {
+    #     'image_path': "40777060/40777060_frames/lowres_wide/40777060_98.764.png",
+    #     'embedding': gen_embeddings(Image.open("40777060/40777060_frames/lowres_wide/40777060_98.764.png")),
+    #     'trajectory_data': read_trajectory("40777060/40777060_frames/lowres_wide/40777060_98.764.png")
+    # })
+
+    schema = pa.schema(
+        [
+            pa.field("image_path", pa.string()),
+            pa.field("embedding", pa.list_(pa.float32(), 512)),
+            pa.field("trajectory_data", pa.string()),
+        ]
     )
 
-tbl.add(data)
-tbl.to_pandas()
+    tbl = db.create_table("image_embeddings", schema=schema, mode="overwrite")
+    
+    
 
+    trajectory_data = read_trajectory(trajectory_path)
+    for file in os.listdir(dir_path):
+        if file.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            image_path = os.path.join(dir_path, file)
+            embedding = gen_embeddings(Image.open(image_path))
+            # rel_data = trajectory_data.loc[trajectory_data['image']==file].to_dict(orient='records')[0]
+            # store_data(db, image_path, embedding, rel_data)
+            print(f"Processed and stored data for {image_path}")
 
-#image search testing
-test = load_dataset("CVdatasets/ImageNet15_animals_unbalanced_aug1", split="validation")
+            
+            
+            data = pandas.DataFrame({
+                'image_path': image_path,
+                'embedding': embedding,
+                'trajectory_data': trajectory_data
+                # 'trajectory_data': rel_data
+            })
+            tbl.add(data)
 
-
-
-##get test image
-def image_search(id):
-    print(Animal(test[id]["labels"]).name)
-    display(test[id]["img"])
-
-    res = tbl.search(gen_embeddings(test[id]["img"])).limit(5).to_df()
-    print(res)
-    for i in range(5):
-        print(Animal(res["label"][i]).name)
-        data_id = int(res["id"][i])
-        display(testImagePath[data_id]["img"])
-
-
-embs = gen_embeddings(test[100]["img"])
-
-res = tbl.search(embs).limit(1).to_df()
-print(res)
-
-print(Animal(res["label"][0]).name)
-id = int(res["id"][0])
-print(id)
+process_images("40777060/40777060_frames/lowres_wide/", "40777060/40777060_3dod_annotation.json")
 
 
 
-def image_search(id):
-    print(Animal(test[id]["labels"]).name)
-    display(test[id]["img"])
 
-    res = tbl.search(gen_embeddings(test[id]["img"])).limit(5).to_df()
-    print(res)
-    for i in range(5):
-        print(Animal(res["label"][i]).name)
-        data_id = int(res["id"][i])
-        display(testImagePath[data_id]["img"])
 
-print(image_search(1200))
+
+
+
+
+
+# # set up lancedb table
+
+
+# db = lancedb.connect("./data/tables")
+# schema = pa.schema(
+#     [
+#         pa.field("vector", pa.list_(pa.float32(), 512)),
+#         pa.field("id", pa.int32()),
+#         pa.field("label", pa.int32()),
+#     ]
+# )
+# tbl = db.create_table("images", schema=schema, mode="overwrite")
+
+# data = []
+# for i in tqdm(range(1, len(testImagePath))):
+#     data.append(
+#         {"vector": gen_embeddings(testImagePath[i]["img"]), "id": i, "label": testImagePath[i]["labels"]}
+#     )
+
+# tbl.add(data)
+# tbl.to_pandas()
+
+
+# #image search testing
+# test = load_dataset("CVdatasets/ImageNet15_animals_unbalanced_aug1", split="validation")
+
+
+
+# ##get test image
+# def image_search(id):
+#     print(Animal(test[id]["labels"]).name)
+#     display(test[id]["img"])
+
+#     res = tbl.search(gen_embeddings(test[id]["img"])).limit(5).to_df()
+#     print(res)
+#     for i in range(5):
+#         print(Animal(res["label"][i]).name)
+#         data_id = int(res["id"][i])
+#         display(testImagePath[data_id]["img"])
+
+
+# embs = gen_embeddings(test[100]["img"])
+
+# res = tbl.search(embs).limit(1).to_df()
+# print(res)
+
+# print(Animal(res["label"][0]).name)
+# id = int(res["id"][0])
+# print(id)
+
+
+
+# def image_search(id):
+#     print(Animal(test[id]["labels"]).name)
+#     display(test[id]["img"])
+
+#     res = tbl.search(gen_embeddings(test[id]["img"])).limit(5).to_df()
+#     print(res)
+#     for i in range(5):
+#         print(Animal(res["label"][i]).name)
+#         data_id = int(res["id"][i])
+#         display(testImagePath[data_id]["img"])
+
+# print(image_search(1200))
 
 # def load_image(path):
 #     image = cv2.imread(path)
 #     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
 #     return image
 
-def embed_txt(txt):
-    text = clip.tokenize([txt]).to(device)
-    embs = model.encode_text(text)
-    return embs.detach().cpu().numpy()[0].tolist()
+# def embed_txt(txt):
+#     text = clip.tokenize([txt]).to(device)
+#     embs = model.encode_text(text)
+#     return embs.detach().cpu().numpy()[0].tolist()
 
-print(len(embed_txt("Black and white dog")))
+# print(len(embed_txt("Black and white dog")))
 
-def text_search(text):
-    res = tbl.search(embed_txt(text)).limit(5).to_df()
-    print(res)
-    Image.show(res)
-    for i in range(len(res)):
-        print(Animal(res["label"][i]).name)
-        data_id = int(res["id"][i])
-        display(testImagePath[data_id]["img"])
+# def text_search(text):
+#     res = tbl.search(embed_txt(text)).limit(5).to_df()
+#     print(res)
+#     for i in range(len(res)):
+#         print(Animal(res["label"][i]).name)
+#         data_id = int(res["id"][i])
+#         display(testImagePath[data_id]["img"])
 
 
-text_search("a full white dog")
-
-# reads trajectory data file
-def read_trajectory(path):
-    trajectory_data = pandas.read_csv(path)
-    return trajectory_data
-
-def store_data(db, image_path, embeddings, trajectory_data):
-    db.insert('image_embeddings', {
-        'image_path': image_path,
-        'embedding': embeddings.tolist(),
-        'trajectory_data': trajectory_data
-    })
-
-def process_images(dir_path, trajectory_path):
-    db = lancedb.LanceDb('embeddings.db')
-    db.create_table('image_embeddings', columns=['image_path', 'embedding', 'trajectory_data'])
-    trajectory_data = read_trajectory(trajectory_path)
-    for file in os.listdir(dir_path):
-        if file.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
-            image_path = os.path.join(dir_path, file)
-            embedding = gen_embeddings(image_path)
-            rel_data = trajectory_data.loc[trajectory_data['image']==file].to_dict(orient='records')[0]
-            store_data(db, image_path, embedding, rel_data)
-            print(f"Processed and stored data for {image_path}")
+# text_search("a full white dog")

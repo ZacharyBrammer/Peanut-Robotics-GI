5db725a8d30e0f4cac3100ae55d9c7c0c7544176
carly11110 <carlyontravel@gmail.com>
2024-07-05 16:06:07 -0400

    Update Display.py

--- Display.py
+++ Display.py
@@ -14,23 +14,26 @@ st.set_page_config(
 
 # Page Title
 st.title("Lorem ipsum dolor!")
+col1, col2 = st.columns(2)
 
 # Create a form
-with st.form("prompt"):
-    st.write("Prompt goes here:")
-    Prompt = st.text_input("Prompt")
-    # Every form must have a submit button
-    submitted = st.form_submit_button("Submit")
+with col1:
+    with st.form("prompt"):
+        st.write("Prompt goes here:")
+        Prompt = st.text_input("Prompt")
+        # Every form must have a submit button
+        submitted = st.form_submit_button("Submit")
     
-    if submitted:
-        st.write(Prompt)
-        embedPrompt = embed_txt(Prompt)
-        out = "static/1.jpg" #placeholder. output goes here
-        # Use columns to place the image and graph side by side
-        col1, col2 = st.columns(2)
+        if submitted:
+           st.write(Prompt)
+           embedPrompt = embed_txt(Prompt)
+           out = "static/1.jpg" #placeholder. output goes here
+           # Use columns to place the image and graph side by side
+
+           st.image(out, caption= out + ", an image of " + Prompt)
         
-        with col1:
-            st.image(out, caption= out + ", an image of " + Prompt)
+
         
-        with col2:
+with col2:
+        if submitted:
             graphTraj(0, 0)  # 0,0 is a placeholder and will be replaced

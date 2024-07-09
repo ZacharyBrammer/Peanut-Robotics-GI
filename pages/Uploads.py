222b866a9f85914af0b7c3940004306dc6474bc6
nima-bhavansikar <nimairene@gmail.com>
2024-07-09 14:43:58 -0700

    Co-authored-by: Cabbage <cabbaeg@users.noreply.github.com> Co-authored-by: Microwaved Potato <MicrowavedFood@users.noreply.github.com> Co-authored-by: co16661666 <co16661666@users.noreply.github.com>

--- pages/Uploads.py
+++ pages/Uploads.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import os
+import zipfile
+from Utils import ImageConvert
+
 
 # Define the path to the uploads directory in the root folder
 uploads_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'uploads')
@@ -15,13 +18,17 @@ st.set_page_config(page_title="Uploads")
 
 st.title('Image Upload')
 
-uploaded_files = st.file_uploader("Choose image files", type=["png", "jpg", "jpeg"], accept_multiple_files=True)
+uploaded_files = st.file_uploader("Choose image files", type=["zip"], accept_multiple_files=True)
 
 if uploaded_files:
     for uploaded_file in uploaded_files:
         save_path = os.path.join('uploads', uploaded_file.name)
         with open(save_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
+        databasename= os.path.splitext(uploaded_file.name)[0]
         st.success(f"Saved file: {uploaded_file.name}")
-        st.image('uploads/'+uploaded_file.name, caption='lorem ipsum')
+        with zipfile.ZipFile(uploaded_file, "r") as z:
+            z.extractall('uploads')
+            
+        table = ImageConvert.process_images(os.path.join('./uploads', databasename, databasename+'_frames', 'lowres_wide'), os.path.join('./uploads', databasename, databasename+'_frames', 'lowres_wide.traj'))
 

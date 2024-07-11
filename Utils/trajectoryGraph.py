a22e17f215e27883467d422ce62a852971f35dff
Skyami <jaanvi0403@gmail.com>
2024-07-11 11:18:20 -0700

    updated trajectory graph

--- Utils/trajectoryGraph.py
+++ Utils/trajectoryGraph.py
@@ -11,11 +11,11 @@ def graphTraj(x,y, file_path, imgname):
     print(file_path.split("/")[1])
     print(imgname)
     wx, wy = wc.load_img_info(file_path.split("/")[1], imgname)
-    
+
     if (wx == None or wy == None):
         print("Error: Could not load image info")
         st.write('Confidence too low to find exact location.')
-        return None
+
     
 
         
@@ -34,8 +34,8 @@ def graphTraj(x,y, file_path, imgname):
     plt.figure(figsize=(10, 7))
     plt.plot(x_positions, y_positions, marker='s', ms=5, mfc='b',mec='b', color='c', label='Trajectory')
     plt.plot(x, y,marker='*', ms='15', mec='r', mfc='r', label = 'Position of camera')
-    
-    plt.plot(wx, wy, marker = 's',mfc='g',mec='g', label= 'object location') #3d coords
+    if (wx != None and wy != None):
+        plt.plot(wx, wy, marker = 's',mfc='g',mec='g', label= 'object location') #3d coords
     
     plt.xlabel('X Position')
     plt.ylabel('Y Position')
